c20a88d8e43dee5430004d1044da8460a8e33463
Satyam Garg <113013930+garg2002@users.noreply.github.com>
2023-10-06 21:47:48 +0530

    Integer Break LeetCode POTD

---
+++ Integer Break/integer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int integerBreak(int n) {
+        int result = 0;
+        for (int k = 2; k <= n; k++) {
+            int mi = n / k;
+            int rest = n % k;
+            int product = pow(mi + 1, rest) * pow(mi, k - rest);
+            result = max(result, product);
+        }
+        return result;
+    }
+int main()
+{
+    int n;
+    cout<<"Enter the Integer:- ";
+    cin>>n;
+    int result  = integerBreak(n);
+    cout<<"Result:-"<<result;
+}
